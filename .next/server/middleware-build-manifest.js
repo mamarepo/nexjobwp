743665cce05e1520e37895c598227cbc797f7388
mamarepo <mamat@glori-ap.co.id>
2025-06-29 20:04:07 +0700

    Updated trace

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,6 +24,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
+    "/artikel": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/artikel.js"
+    ],
     "/lowongan-kerja": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
