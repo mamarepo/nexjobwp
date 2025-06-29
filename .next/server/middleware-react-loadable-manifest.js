0c6b0a12f029494918176180c30764fc02386f2e
mamarepo <mamat@glori-ap.co.id>
2025-06-29 20:06:18 +0700

    Deleted package-lock.json

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages/_app.tsx -> @/services/wpService\":{\"id\":\"pages/_app.tsx -> @/services/wpService\",\"files\":[\"static/chunks/src_services_wpService_ts.js\"]}}"
